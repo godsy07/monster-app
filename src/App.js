525445959717e5d1a7e2712f6afc74e07365a83a
Godson <godson@incrediblevisibility.com>
2025-06-13 17:51:06 +0530

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import {CardList} from './components/card-list/card-list.component';
-import {SearchBox} from './components/search-box/search-box.component'
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {      
+    this.state = {
       monsters: [],
-      searchField: '',
-      title: ''
+      searchField: "",
+      title: "",
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters:users}));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  onSearchChange = e => {
-    this.setState({searchField: e.target.value})
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value });
   };
 
-  render () {
-    const {monsters,searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
 
-        <SearchBox 
-          // placeholder="search monsters" 
-          placeholder="Search your monster" 
+        <SearchBox
+          // placeholder="search monsters"
+          placeholder="Search your monster"
           onSearchChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
-          
       </div>
     );
-  };
+  }
 }
 
 export default App;
